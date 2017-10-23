8f0edaae112cb45414db10984da2f156ed8d8e32
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2017-10-23 21:21:50 -0200

    Module4-Solution

--- modulo4-solution/harder/SpeakHello.js
+++ modulo4-solution/harder/SpeakHello.js
@@ -8,12 +8,14 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+(function () {
+var helloSpeaker = {};
 var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
@@ -22,3 +24,6 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+window.helloSpeaker = helloSpeaker;
+})();
